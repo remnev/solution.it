cb8333d41753c0852c105b12e8f946d971b1e9a2
Yuriy Remnev <yura_remnev@mail.ru>
2014-04-08 11:42:39 +0400

    Добавил карусель

--- routes/views/index.js
+++ routes/views/index.js
@@ -2,11 +2,23 @@ var keystone = require('keystone');
 
 exports = module.exports = function(req, res) {
     var locals = res.locals,
-        view = new keystone.View(req, res);
+        view = new keystone.View(req, res),
+        PortfolioProject = keystone.list('PortfolioProject');
 
     // Set locals
     locals.section = 'company';
 
-    // Render the view
-    view.render('index');
+    PortfolioProject.model
+        .find()
+        .where({ state: 'published' })
+        .select('title image linkTo')
+        .exec(function(err, portfolioProjects) {
+            if (err) throw err;
+
+            locals.portfolioProjects = portfolioProjects;
+
+            // Render the view
+            view.render('index');
+        });
+
 }
